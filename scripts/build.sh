6ee5e2aad6bc5aec3e71571894ad44a5dc684725
Roelf Wichertjes <roelf@roelf.org>
2019-02-22 10:53:47 +0100

    Initial cmakefile

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ "$(id -u)" == "0" ]; then
+   echo "This script must not be run as root" 1>&2
+   exit 1
+fi
+
+cd "$(dirname "$(realpath "$0")")"/..
+
+mkdir -p build
+cd build
+cmake ..
+exec make
+
