1e8996a78eafe17acd351b23bcd8faccc2748745
Roelf Wichertjes <roelf@roelf.org>
2019-03-20 13:54:43 +0100

    Random C++ file for assembly testing.

---
+++ tools/random.cpp
@@ -0,0 +1,18 @@
+//
+// Created by rewbycraft on 3/20/19.
+//
+
+#include "parser/internal.h"
+#include <boost/fusion/include/adapt_struct.hpp>
+
+struct test_t {
+	uint32_t a,b;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(test_t, (uint32_t, a), (uint32_t, b))
+
+struct test_t test(parser::bytevector& vec) {
+	struct test_t t;
+	parser::deserializeObject(t, vec);
+	return t;
+}
